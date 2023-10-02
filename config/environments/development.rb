b0865436539988f356a5c5e375d3e262dc88332a
EC2 Default User <ec2-user@ip-172-31-11-179.ap-northeast-1.compute.internal>
2023-10-02 04:34:38 +0000

    Homesコントローラを作成した

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "714763aa669a4c23ad9d80e3a08dfaaf.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
