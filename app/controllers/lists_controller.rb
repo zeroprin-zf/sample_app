869c4e135bcb806b4c7ce3a21676cc94ab21055a
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-03 01:26:52 +0000

    [Finish] 3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,24 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
 
-  def index
+
+  def create
+    list = List.new(list_params)
+
+    list.save
+
+    redirect_to '/top'
   end
 
   def show
   end
 
   def edit
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
