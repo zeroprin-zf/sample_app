333facddb672834b40b80913a5894137df1a3987
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-04 02:37:14 +0000

    show.html.erb作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,8 +15,9 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
-  
+
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  get 'lists/edit'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get '/top' => 'homes#top'
+  get 'lists/:id' => 'lists#show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
