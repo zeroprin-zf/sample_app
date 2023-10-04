8f708790d54abb21f57a6784bfcdf1ef7da1426c
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-04 08:22:00 +0000

    ビューに記述を追加するまで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,7 +19,14 @@ class ListsController < ApplicationController
   end
 
   def edit
-    @list = List.find(params[:])
+    @list = List.find(params[:id])
+  end
+
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   get 'lists/:id' => 'lists#show' , as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
