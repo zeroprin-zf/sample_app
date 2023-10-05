bea10298801c41d2973e6e7f11bf1d778a278dfb
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-05 02:18:35 +0000

    [Finish]3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,8 +9,7 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list.id)
     else
-      @lists = List.all
-      render :index
+      render :new
     end
   end
 
