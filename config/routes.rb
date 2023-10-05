a7c395c6db6c200aa7d8f1a524ea49dea0f8cd77
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-05 07:22:38 +0000

    [Finish]3-11

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'top' => 'home#top'
+
+  get 'top' => 'homes#top'
   resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
